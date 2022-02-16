e57201b57218db946e3d1e653973f172040b4981
mcthoren <thoren.mcdole@gmail.com>
2022-02-16 18:29:49 +0000

    actually write the lock, and in the correct place. this is what i get for being in a hurry on a remote maschine.

--- wx.combi.go
+++ wx.combi.go
@@ -2,12 +2,15 @@
 
 SRC_D='/home/ghz/repos/nw.wx'
 DST_D='/var/www/htdocs/darkdata.org/ghz/nw.wx'
-LOCK="${DST_D}/nw.wx.lock"
+LOCK="${SRC_D}/nw.wx.lock"
 
 [ -e "${LOCK}" ] && {
 	echo "$0: lock exists" | logger
 	exit 1
 }
+
+# lock is also checked for and deleted on boot, in case of a crash
+touch "${LOCK}"
 
 cd "${SRC_D}" || exit 1
 /usr/local/bin/gnuplot wx.gnuplot
